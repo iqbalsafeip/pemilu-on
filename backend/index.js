5804c32ecd8eb1b070d7e8a3286146a5dc8e3135
iqbalsafeip <isaacfrost@gmail.com>
2020-04-06 07:20:44 +0700

    pusing

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const server = require('https').createServer(app);
+const server = require('http').createServer(app);
 require('dotenv').config();
 
 // middleware
